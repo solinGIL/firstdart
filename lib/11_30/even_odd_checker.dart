bf564f60b2e410dde331613ea78c60b7e522b50d
solin <rhkrvkfk@hotmail.com>
2023-11-30 10:38:56 +0900

    짝수 홀수 구별

--- lib/11_30/even_odd_checker.dart
+++ lib/11_30/even_odd_checker.dart
@@ -1,12 +1,16 @@
-import 'dart:io';
-
 main() {
-  print('숫자를 입력하세요: ');
-  int number = int.parse(stdin.readLineSync()!);
+  // 미션 1: 30과 15
+  checkEvenOdd(30, 15);
 
-  if (number % 2 == 0) {
-    print('$number 은(는) 짝수입니다.');
+  // 미션 2: 12와 40
+  checkEvenOdd(12, 40);
+}
+
+void checkEvenOdd(int n, int m) {
+  if ((n % 2 == 0 && m % 2 != 0) || (n % 2 != 0 && m % 2 == 0)) {
+    print('YES');
   } else {
-    print('$number 은(는) 홀수입니다.');
+    print('NO');
   }
 }
+
